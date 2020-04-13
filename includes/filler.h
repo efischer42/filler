76420777ea1c2c266f86e91b279fa7c4c2cf7bc7
MilouFischer <etienne.fischer77@gmail.com>
2020-04-13 22:51:48 +0200

    able to test on ubuntu

--- includes/filler.h
+++ includes/filler.h
@@ -3,4 +3,12 @@
 
 # include "libft.h"
 
+# define STDIN	0
+# define STATE_CHECK 0x1
+
+typedef struct	s_machine
+{
+	uint64_t	state;
+}				t_machine;
+
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -2,8 +2,17 @@
 
 int		main(int ac, char **av)
 {
+	char	*line;
+
 	(void)ac;
 	(void)av;
-	ft_putendl("Everything's well working !");
+	line = NULL;
+	get_next_line(STDIN, &line);
+	ft_putendl_fd("");
+	while (get_next_line(STDIN, &line) == TRUE)
+	{
+		ft_putendl_fd(line, 2);
+		ft_strdel(&line);
+	}
 	return (EXIT_SUCCESS);
 }
